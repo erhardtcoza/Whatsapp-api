abe7622e5401fa5b01768d39b9dff16436a63ef5
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-07-10 16:33:03 +0200

    Update cors.js

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,1 +1,12 @@
+// cors.js - Utility for CORS response handling
 
+export function withCORS(response) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "*");
+  return response;
+}
+
+export function handleOptions(request) {
+  return withCORS(new Response(null, { status: 204 }));
+}
